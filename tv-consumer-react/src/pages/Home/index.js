0d7e21b7fd0092b25fe63dd1e7c33f1bafc743a6
José Darci Rodrigues Junior <juniorinternet@gmail.com>
2021-08-12 13:16:21 -0300

    Coments in index

--- tv-consumer-react/src/pages/Home/index.js
+++ tv-consumer-react/src/pages/Home/index.js
@@ -4,7 +4,8 @@ import FeaturedMovie from "../../components/FeaturedMovie";
 import "./styles.css";
 
 // import { Container } from './styles';
-
+// FeaturedMovie is a component more important because show episode selected.
+// EpisodesRow is a second component , your function is renderized a more miniatures od episodes of girls power
 function Home() {
   return (
     <>
